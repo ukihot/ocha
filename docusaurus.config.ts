f87ba629d823ed69147d6a5593247890e952a542
ukihot <tokunaga.y@tyco.co.jp>
2023-12-20 10:13:48 +0900

    :wrench: update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: "https://github.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: ".",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "tyco", // Usually your GitHub org/user name.
+  projectName: "ocha", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
