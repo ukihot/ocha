0a12378e674c8998618a6f20c5282bfbbcce787c
ukihot <tokunaga.y@tyco.co.jp>
2023-12-20 10:54:13 +0900

    :construction: wip

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,18 +8,19 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://ukihot.github.io",
+  url: "https://ukihot.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/ocha/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "tyco", // Usually your GitHub org/user name.
+  organizationName: "ukihot", // Usually your GitHub org/user name.
   projectName: "ocha", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
